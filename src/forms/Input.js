c25563a833fd04e7b62945b00a6c01b808cbbe9f
Mincho <mincho.milev@gmail.com>
2022-09-26 10:06:23 +0300

    style

--- src/forms/Input.js
+++ src/forms/Input.js
@@ -4,8 +4,8 @@ import classes from './Input.module.css';
 
 const Input = (props) => {
   return (
-    <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.placeholder}</label>
+    <div className={classes.input + ' ' + classes.contentInput}>
+      <label className={classes.labelStyle} htmlFor={props.input.id}>{props.placeholder}</label>
       <input onChange={props.changeValue}  {...props.input} onBlur={props.onBlur} />
       {props.hasError && <p className={classes.errorText}>{props.placeholder} must not be empty</p>}
     </div>
--- src/forms/TextArea.js
+++ src/forms/TextArea.js
@@ -4,8 +4,8 @@ import classes from './Input.module.css';
 
 const TextArea = (props) => {
   return (
-    <div className={classes.input}>
-      <label htmlFor={props.input.id}>{props.placeholder}</label>
+    <div className={classes.input  + ' ' + classes.contentInput}>
+      <label className={classes.labelStyle} htmlFor={props.input.id}>{props.placeholder}</label>
       <textarea onChange={props.changeValue} {...props.input} onBlur={props.onBlur} />
       {props.hasError && <p className={classes.errorText}>{props.placeholder} must not be empty</p>}
     </div>
